fe10e42588ebce59a5bfb41494859827db6770b1
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2023-09-08 17:33:09 -0300

    experimentando mudança de temas

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-const themes = { 
-    light: {
-      background: 'white',
-      text: 'black',
-    },
-    dark: {
-      background: 'black',
-      text: 'white',
+var cordofundo = "branca";
+
+function mudartema() {
+    var body = document.body;
+    var elementosDeTexto = document.querySelectorAll('h1, h2, p');
+    var elementosDoHeader = document.querySelectorAll('header *'); // Seleciona todos os elementos dentro do header
+    var divisao = document.querySelector('.divisao'); // Seleciona a linha de divisão
+
+    if (cordofundo === 'branca') {
+        body.style.background = '#0C0F33';
+        cordofundo = 'roxa';
+
+        // Altere a cor do texto para branco
+        elementosDeTexto.forEach(function(elemento) {
+            elemento.style.color = 'white';
+        });
+
+        // Altere a cor do texto dos elementos dentro do header para branco
+        elementosDoHeader.forEach(function(elemento) {
+            elemento.style.color = 'white';
+        });
+
+        // Altere a cor da linha de divisão para branco
+        divisao.style.backgroundColor = 'white';
+    } else {
+        body.style.background = 'white';
+        cordofundo = 'branca';
+
+        // Reset a cor do texto para roxo
+        elementosDeTexto.forEach(function(elemento) {
+            elemento.style.color = 'purple';
+        });
+
+        // Reset a cor do texto dos elementos dentro do header para roxo
+        elementosDoHeader.forEach(function(elemento) {
+            elemento.style.color = 'purple';
+        });
+
+        // Reset a cor da linha de divisão para roxo
+        divisao.style.backgroundColor = 'purple';
     }
-  };
-
-  function setTheme(newTheme) {
-    const themeColors = themes[newTheme]; // Seleciona o tema para aplicar
-  
-    Object.keys(themeColors).map(function(key) {
-      html.style.setProperty(`--${key}`, themeColors[key]); // Altera as variáveis no css
-    });
-  }
-
-  <input name="theme" type="checkbox" class="toggle-dark-mode" />
-
-  const darkModeToggle = document.querySelector('.toggle-dark-mode');
-
-darkModeToggle.addEventListener('change', function({ target }) {
-  setTheme(target.checked ? 'dark' : 'light');
-});
+}
